a3097d0aadfa122cf861b7acd2fdd4c37d2258bd
Yony Marian <yonyon.marian@gmail.com>
2024-12-27 17:05:23 -0800

    implement User entity

--- src/main/java/com/yonymarian/recipe_api_demo/entity/User.java
+++ src/main/java/com/yonymarian/recipe_api_demo/entity/User.java
@@ -2,14 +2,25 @@ package com.yonymarian.recipe_api_demo.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.NonNull;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Data
 @Entity
 @Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "recipe")
+    private Set<Recipe> recipes;
+
 
 
 }
