a1864e0e0cfe48a619d7d5de05151e60901dc702
Yony Marian <yonyon.marian@gmail.com>
2024-12-28 19:55:22 -0800

    change Optionals to Lists

--- src/main/java/com/yonymarian/recipe_api_demo/repository/RecipeRepository.java
+++ src/main/java/com/yonymarian/recipe_api_demo/repository/RecipeRepository.java
@@ -4,7 +4,6 @@ import com.yonymarian.recipe_api_demo.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface RecipeRepository extends JpaRepository<Recipe, UUID> {
--- src/main/java/com/yonymarian/recipe_api_demo/repository/UserRepository.java
+++ src/main/java/com/yonymarian/recipe_api_demo/repository/UserRepository.java
@@ -3,11 +3,12 @@ package com.yonymarian.recipe_api_demo.repository;
 import com.yonymarian.recipe_api_demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    public Optional<User> findByName();
-    public Optional<User> findByRecipes_Name();
+    public List<User> findByName();
+    public List<User> findByRecipes_Name();
 }
