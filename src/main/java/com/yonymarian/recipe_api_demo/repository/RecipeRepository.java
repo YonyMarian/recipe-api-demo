0538bd8771a8437d63356b13a87c1615d0df8133
Yony Marian <yonyon.marian@gmail.com>
2024-12-28 19:50:43 -0800

    add findBy queries to existing Repositories

--- src/main/java/com/yonymarian/recipe_api_demo/repository/RecipeRepository.java
+++ src/main/java/com/yonymarian/recipe_api_demo/repository/RecipeRepository.java
@@ -2,8 +2,18 @@ package com.yonymarian.recipe_api_demo.repository;
 
 import com.yonymarian.recipe_api_demo.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RecipeRepository extends JpaRepository<Recipe, UUID> {
+
+    public List<Recipe> findByName();
+    public List<Recipe> findByNameAndAuthor();
+    public List<Recipe> findByAuthor();
+    public List<Recipe> findByDifficulty();
+    public List<Recipe> findByIngredients_IngredientName();
+    public List<Recipe> findByTags_TagName();
 
 }
--- src/main/java/com/yonymarian/recipe_api_demo/repository/UserRepository.java
+++ src/main/java/com/yonymarian/recipe_api_demo/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.yonymarian.recipe_api_demo.repository;
 
 import com.yonymarian.recipe_api_demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    public Optional<User> findByName();
+    public Optional<User> findByRecipes_Name();
 }
