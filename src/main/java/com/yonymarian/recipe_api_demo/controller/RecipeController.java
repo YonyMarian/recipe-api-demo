0dd2b2ffeed414081ff5b6558939cd3bb18f3da8
Yony Marian <yonyon.marian@gmail.com>
2024-12-29 13:03:16 -0800

    set up controller classes

--- src/main/java/com/yonymarian/recipe_api_demo/controller/RecipeController.java
+++ src/main/java/com/yonymarian/recipe_api_demo/controller/RecipeController.java
@@ -1,9 +1,8 @@
 package com.yonymarian.recipe_api_demo.controller;
 
 
-import com.yonymarian.recipe_api_demo.repository.RecipeRepository;
+import com.yonymarian.recipe_api_demo.service.RecipeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 @RequiredArgsConstructor
 public class RecipeController {
-    private final RecipeRepository recipeRepository;
+    private final RecipeService recipeService;
 
 
 }
--- src/main/java/com/yonymarian/recipe_api_demo/controller/UserController.java
+++ src/main/java/com/yonymarian/recipe_api_demo/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.yonymarian.recipe_api_demo.controller;
 
 
-import com.yonymarian.recipe_api_demo.repository.UserRepository;
+import com.yonymarian.recipe_api_demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 @RequiredArgsConstructor
 public class UserController {
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     
 }
